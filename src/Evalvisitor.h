d6db17017a995256ed26f0705bf3b4a381edec6d
qweryy0566 <qweryy0566@qq.com>
2021-11-03 16:46:23 +0800

    first commit

--- src/Evalvisitor.h
+++ src/Evalvisitor.h
@@ -1,19 +1,6 @@
 #ifndef PYTHON_INTERPRETER_EVALVISITOR_H
 #define PYTHON_INTERPRETER_EVALVISITOR_H
 
-
-#include "Python3BaseVisitor.h"
-
-
-class EvalVisitor: public Python3BaseVisitor {
-
-//todo:override all methods of Python3BaseVisitor
-};
-
-
-#endif //PYTHON_INTERPRETER_EVALVISITOR_H#ifndef PYTHON_INTERPRETER_EVALVISITOR_H
-#define PYTHON_INTERPRETER_EVALVISITOR_H
-
 #include "Python3BaseVisitor.h"
 
 class EvalVisitor: public Python3BaseVisitor {
